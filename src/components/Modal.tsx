d360b90a0a3addf79c70f62d1f2448e4dcf81650
IO <limlimgo14@gmail.com>
2026-02-18 22:39:44 +0800

    add animation

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,7 @@
 import { useEffect, type ReactNode } from "react";
 import { createPortal } from "react-dom";
 import { RiCloseLargeFill } from "react-icons/ri";
+import { motion, AnimatePresence } from "motion/react";
 
 type Props = {
   open: boolean;
@@ -25,24 +26,34 @@ const Modal = ({ children, open, onClose }: Props) => {
     };
   }, [open, onClose]);
 
-  if (!open) return null;
-
   return createPortal(
-    <div
-      className="fixed top-0 left-0 w-full h-screen bg-[#00000090] flex items-center justify-center z-10"
-      onClick={onClose}
-    >
-      <div
-        className="bg-white w-186 p-6 px-25 flex items-center justify-start flex-col relative"
-        onClick={(e) => e.stopPropagation()}
-      >
-        <RiCloseLargeFill
-          className="absolute top-4 right-4 cursor-pointer"
+    <AnimatePresence>
+      {open && (
+        <motion.div
+          className="fixed top-0 left-0 w-full h-screen bg-[#00000090] flex items-center justify-center z-50"
           onClick={onClose}
-        />
-        {children}
-      </div>
-    </div>,
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.2 }}
+        >
+          <motion.div
+            className="bg-white w-186 p-6 px-25 flex items-center justify-start flex-col relative"
+            onClick={(e) => e.stopPropagation()}
+            initial={{ scale: 0.9, opacity: 0, y: 20 }}
+            animate={{ scale: 1, opacity: 1, y: 0 }}
+            exit={{ scale: 0.9, opacity: 0, y: 20 }}
+            transition={{ duration: 0.25, ease: "easeOut" }}
+          >
+            <RiCloseLargeFill
+              className="absolute top-4 right-4 cursor-pointer"
+              onClick={onClose}
+            />
+            {children}
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>,
     document.getElementById("root")!,
   );
 };

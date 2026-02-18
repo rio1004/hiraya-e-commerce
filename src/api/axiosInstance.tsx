7070077b898ee02ec81e2a5eaf64c3e66d833e24
IO <limlimgo14@gmail.com>
2026-02-18 17:57:27 +0800

    Fix the uri problem

--- src/api/axiosInstance.tsx
+++ src/api/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios, { type InternalAxiosRequestConfig } from "axios";
 import { useAuthStore } from "@/stores/auth/useAuth";
 export const axiosInstance = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 30000,
 });
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,13 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  const API_URL = env.VITE_API_URL;
-  console.log(API_URL);
-  return {
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    server: {
-      proxy: {
-        "/api": {
-          target: "https://hiraya-be.onrender.com",
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ""),
-        },
-      },
-    },
-  };
+  },
 });
